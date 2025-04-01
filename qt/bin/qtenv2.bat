0fce53b09770a4ae0ee9da47f42af10ecd7117a1
Jakub Vaněk <linuxtardis@gmail.com>
2025-04-02 00:31:27 +0200

    Add Qt 5.6.3

---
+++ qt/bin/qtenv2.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=C:\Qt\zeroelab_qt563\5.6.3\mingw49_32\bin;C:/qt/zeroelab_qt563/Tools/mingw492_32\bin;%PATH%
+cd /D C:\Qt\zeroelab_qt563\5.6.3\mingw49_32
